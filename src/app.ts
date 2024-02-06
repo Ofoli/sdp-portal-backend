12333e41209e26cb8a06f8b09700dbd963c36e47
Lawrence Ofoli Mensah <lawrenceofoli@gmail.com>
2024-02-06 08:31:01 +0000

    added connection to mongo

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
+import dotenv from "dotenv";
 import express, { Request, Response } from "express";
+import { connectToMongoDB } from "./config";
+import { DB_CONN_RETRY_ATTEMPTS } from "./constants";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -7,6 +11,10 @@ app.get("/", (req: Request, res: Response) =>
   res.send({ status: true, message: "App is running now" })
 );
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+connectToMongoDB(DB_CONN_RETRY_ATTEMPTS).then((connected) => {
+  if (connected) {
+    app.listen(PORT, () => {
+      console.log(`Server is running at http://localhost:${PORT}`);
+    });
+  }
 });
