192a160d82e00090405234e105ff40d3a5b6e860
Lawrence Ofoli Mensah <lawrenceofoli@gmail.com>
2024-09-10 18:13:29 +0000

    remove cors

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,15 +1,9 @@
 import path from "path";
 
-const FE_URL_DEV = "http://localhost:5173";
-const FE_URL_LIVE = "https://sandboxes.nalosolutions.com";
-
 export const ENV_FILE_PATH = path.join(__dirname, "..", "..", ".env");
 export const MONGO_DB_HOST = "mongodb://db";
 export const STATUSES = { SUCCESS: true, FAILED: false };
-export const CORS_OPTIONS = {
-  origin: [FE_URL_DEV, FE_URL_LIVE],
-  credentials: true,
-};
+
 export const URLS = {
   base: "/",
   auth: "/api/auth",
