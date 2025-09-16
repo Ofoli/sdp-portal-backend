614ea0a641fe8a584c322fa02db8508630bb60e8
Lawrence Ofoli Mensah <lawrenceofoli@gmail.com>
2025-09-16 08:24:07 +0000

    connect to sdb using ssh tunnel

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,28 +1,55 @@
+import { z } from "zod";
 import dotenv from "dotenv";
-import { MONGO_DB_HOST, ENV_FILE_PATH } from "./constants";
+import { ENV_FILE_PATH } from "./constants";
 
 dotenv.config({ path: ENV_FILE_PATH });
 
-const dbPort = process.env.DB_PORT;
-const database = process.env.DB_NAME;
-const databaseUri = `${MONGO_DB_HOST}:${dbPort}/${database}`;
+const envSchema = z.object({
+  PORT: z.number({ coerce: true }),
+  DB_HOST: z.string(),
+  DB_NAME: z.string(),
+  DB_PORT: z.string(),
+  SDB_HOST: z.string(),
+  SDB_NAME: z.string(),
+  SDB_USER: z.string(),
+  SDB_PASS: z.string(),
+  SSH_HOST: z.string(),
+  SSH_USER: z.string(),
+  SSH_KEY_PATH: z.string(),
+  PASSWORD_SALT: z.string(),
+  JWT_TOKEN_SECRET: z.string(),
+});
 
-const config = {
-  PORT: process.env.PORT,
-  DB: {
-    URI: databaseUri,
-    CONN_RETRY_INTERVAL: 1000,
-    CONN_RETRY_ATTEMPTS: 5,
-  },
-  SDB: {
-    USERNAME: process.env.SDB_USERNAME!,
-    PASSWORD: process.env.SDB_PASSWORD!,
-    HOST: process.env.SDB_HOST!,
-    NAME: process.env.SDB_NAME!,
-    PORT: 3306,
-  },
-  PASSWORD_SALT: process.env.PASSWORD_SALT!,
-  JWT_TOKEN: { SECRET: process.env.JWT_TOKEN_SECRET!, EXPIRATION: "30m" },
-};
+function parseEnv(processEnv: NodeJS.ProcessEnv) {
+  try {
+    const env = envSchema.parse(processEnv);
+    const databaseUri = `${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}`;
+    return {
+      PORT: env.PORT,
+      DB: {
+        URI: databaseUri,
+        CONN_RETRY_INTERVAL: 1000,
+        CONN_RETRY_ATTEMPTS: 5,
+      },
+      SDB: {
+        USERNAME: env.SDB_USER,
+        PASSWORD: env.SDB_PASS,
+        HOST: env.SDB_HOST,
+        NAME: env.SDB_NAME,
+        PORT: 3306,
+      },
+      SSH: {
+        HOST: env.SSH_HOST,
+        USER: env.SSH_USER,
+        PRIVATE_KEY_PATH: env.SSH_KEY_PATH!,
+      },
+      PASSWORD_SALT: env.PASSWORD_SALT,
+      JWT_TOKEN: { SECRET: env.JWT_TOKEN_SECRET, EXPIRATION: "30m" },
+    };
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+}
 
-export { config };
+export const config = parseEnv(process.env);
--- src/utils/sdb.ts
+++ src/utils/sdb.ts
@@ -1,31 +1,109 @@
-import mysql from "mysql2/promise";
+import fs from "fs";
+import mysql, { Connection } from "mysql2/promise";
+import { Client } from "ssh2";
 import { config } from "../config/config";
 import { logger } from "./logger";
 import { STATUSES } from "../config/constants";
 import type { SdpReport } from "../types/report";
 
-type SdbConfig = typeof config.SDB;
+type ShortcodeConfig = {
+  db: typeof config.SDB;
+  ssh: typeof config.SSH;
+};
 
-class ShortcodeDB {
-  private config: SdbConfig;
+class ShortcodeDbConnector {
+  private config: ShortcodeConfig;
 
-  constructor(config: any) {
+  constructor(config: ShortcodeConfig) {
     this.config = config;
   }
 
-  private getConnection = async () => {
-    return await mysql.createConnection({
-      host: this.config.HOST,
-      database: this.config.NAME,
-      user: this.config.USERNAME,
-      password: this.config.PASSWORD,
-      port: this.config.PORT,
+  private establishSshTunnel(): Promise<Client> {
+    return new Promise((resolve, reject) => {
+      const conn = new Client();
+
+      conn
+        .on("ready", () => {
+          logger.info("SSH tunnel established.");
+          resolve(conn);
+        })
+        .on("error", (err) => {
+          reject(err);
+        })
+        .connect({
+          host: this.config.ssh.HOST,
+          username: this.config.ssh.USER,
+          privateKey: fs.readFileSync(this.config.ssh.PRIVATE_KEY_PATH),
+        });
     });
-  };
+  }
+
+  private handleForwarding(
+    resolve: (connection: Connection) => void,
+    reject: (err: Error) => void
+  ) {
+    return async (err: Error | undefined, stream: NodeJS.ReadWriteStream) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      try {
+        const connection = await mysql.createConnection({
+          host: this.config.db.HOST,
+          port: this.config.db.PORT,
+          user: this.config.db.USERNAME,
+          database: this.config.db.NAME,
+          password: this.config.db.PASSWORD,
+          stream,
+        });
+        resolve(connection);
+      } catch (dbErr) {
+        reject(dbErr as Error);
+      }
+    };
+  }
+
+  private createDbConnection(sshClient: Client): Promise<Connection> {
+    return new Promise((resolve, reject) => {
+      sshClient.forwardOut(
+        this.config.db.HOST,
+        this.config.db.PORT,
+        this.config.db.HOST,
+        this.config.db.PORT,
+        this.handleForwarding(resolve, reject)
+      );
+    });
+  }
+
+  public async connect() {
+    try {
+      const sshClient = await this.establishSshTunnel();
+      return this.createDbConnection(sshClient);
+    } catch (err) {
+      const { message } = err as Error;
+      logger.error({
+        action: "get sdb connection",
+        details: message,
+      });
+      return null;
+    }
+  }
+}
+
+class ShortcodeDB {
+  private dbConnector: ShortcodeDbConnector;
+
+  constructor(connector: ShortcodeDbConnector) {
+    this.dbConnector = connector;
+  }
 
   async uploadSdp(report: SdpReport[]) {
     let uploadStatus = STATUSES.FAILED;
-    const connection = await this.getConnection();
+
+    const connection = await this.dbConnector.connect();
+    if (!connection) return uploadStatus;
+
     const values = this.formatSdpValues(report);
     const sql = `INSERT INTO revenue (network, counts, revenue, service, revenue_date, created_on, shortcode) VALUES ${values}`;
 
@@ -59,6 +137,7 @@ class ShortcodeDB {
   }
 }
 
-const shortcodeDb = new ShortcodeDB(config.SDB);
+const connector = new ShortcodeDbConnector({ db: config.SDB, ssh: config.SSH });
+const shortcodeDb = new ShortcodeDB(connector);
 
 export { shortcodeDb };
